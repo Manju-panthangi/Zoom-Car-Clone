a0adf22405c3d8a51160c71d707ced085e02152b
Manju-panthangi <manjupanthangi33597@gmail.com>
2022-05-22 00:41:04 +0530

    Latest fixes

--- src/main/java/com/manju/zoomcarclone/controllers/CarController.java
+++ src/main/java/com/manju/zoomcarclone/controllers/CarController.java
@@ -1,20 +1,22 @@
 package com.manju.zoomcarclone.controllers;
 
+import com.manju.zoomcarclone.views.CarVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class CarController {
 
     @GetMapping()
-    @PostMapping("/register")
-    void register(){
-
+    List<CarVO> search(){
+        return null;
     }
-
+    
     @PostMapping("/reserve")
     void reserve(){
 
