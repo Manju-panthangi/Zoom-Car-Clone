31bf1de2bd974a9bc1314872cca8d79355c9ed7c
Manju-panthangi <manjupanthangi33597@gmail.com>
2022-05-21 14:30:20 +0530

    Added spring files

---
+++ src/main/java/com/manju/zoomcarclone/ZoomCarCloneApplication.java
@@ -0,0 +1,13 @@
+package com.manju.zoomcarclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomCarCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomCarCloneApplication.class, args);
+	}
+
+}
